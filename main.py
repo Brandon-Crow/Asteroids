460f378982051589fb4186ffd278030f33a96b0e
brandon-crow <brandonacrow@gamil.com>
2025-06-14 17:14:28 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill((0,0,0))  # fill the screen with black
+        pygame.display.flip()  # update the display
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
